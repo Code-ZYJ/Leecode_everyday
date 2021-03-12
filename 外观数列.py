584b09ab45f9af018a190a8694bf9049aa38031a
Code-ZYJ <1103447068@qq.com>
2021-03-12 20:00:22 +0800

    init

---
+++ 外观数列.py
@@ -0,0 +1,53 @@
+'''
+给定一个正整数 n ，输出外观数列的第 n 项。
+
+「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
+
+你可以将其视作是由递归公式定义的数字字符串序列：
+
+countAndSay(1) = "1"
+countAndSay(n) 是对 countAndSay(n-1) 的描述，然后转换成另一个数字字符串。
+前五项如下：
+
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+第一项是数字 1
+描述前一项，这个数是 1 即 “ 一 个 1 ”，记作 "11"
+描述前一项，这个数是 11 即 “ 二 个 1 ” ，记作 "21"
+描述前一项，这个数是 21 即 “ 一 个 2 + 一 个 1 ” ，记作 "1211"
+描述前一项，这个数是 1211 即 “ 一 个 1 + 一 个 2 + 二 个 1 ” ，记作 "111221"
+'''
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        pre = ''
+        cur = '1'
+
+        # 从第 2 项开始
+        for _ in range(1, n):
+            # 这里注意要将 cur 赋值给 pre
+            # 因为当前项，就是下一项的前一项。有点绕，尝试理解下
+            pre = cur
+            # 这里 cur 初始化为空，重新拼接
+            cur = ''
+            # 定义双指针 start，end
+            start = 0
+            end = 0
+            # 开始遍历前一项，开始描述
+            while end < len(pre):
+                # 统计重复元素的次数，出现不同元素时，停止
+                # 记录出现的次数，
+                while end < len(pre) and pre[start] == pre[end]:
+                    end += 1
+                # 元素出现次数与元素进行拼接
+                cur += str(end - start) + pre[start]
+                # 这里更新 start，开始记录下一个元素
+                start = end
+
+        return cur

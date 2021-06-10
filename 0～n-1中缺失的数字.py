a019ab02ba64d62a25394a55d4f681f5b988ab4d
Code-ZYJ <1103447068@qq.com>
2021-06-10 14:14:39 +0800

    init

---
+++ 0～n-1中缺失的数字.py
@@ -0,0 +1,26 @@
+'''
+一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+
+ 
+
+示例 1:
+
+输入: [0,1,3]
+输出: 2
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+# 二分查找
+class Solution():
+    def missingNumber(self, nums):
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            m = (i + j) // 2
+            if nums[m] == m: i = m + 1
+            else: j = m - 1
+        return i
+
+nums = [0,1,3]
+Solution().missingNumber(nums)
